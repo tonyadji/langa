9f1023997b58a8232d5fca25bb9d60a95a2cfcdf
tonyadji <tonysimonadji@gmail.com>
2025-10-12 19:43:25 +0200

    feat(Backend): implement application usage tracking and retrieval
    - Added `GetUsageUseCase` to retrieve application usage data.
    - Introduced `ApplicationUsageInfo` and `ApplicationUsageDto` for usage representation.
    - Updated `ApplicationController` with `/usage` endpoint for fetching usage details.
    - Enhanced `IngestionUseCase` to track log and metric sizes using `IngestionSizeCalculator`.
    - Added `Entry` interface for size calculation in `LogEntry` and `MetricEntry`.
    - Updated CORS configuration to include Swagger-related paths.
    
    TODO :
    - man you have to unit tests the new classes

--- backend/src/main/java/com/langa/backend/infra/persistence/repositories/applications/mongo/daos/MongoApplicationUsageDao.java
+++ backend/src/main/java/com/langa/backend/infra/persistence/repositories/applications/mongo/daos/MongoApplicationUsageDao.java
@@ -3,5 +3,8 @@ package com.langa.backend.infra.persistence.repositories.applications.mongo.daos
 import com.langa.backend.infra.persistence.repositories.applications.mongo.documents.ApplicationUsageDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface MongoApplicationUsageDao extends MongoRepository<ApplicationUsageDocument, String> {
+    Optional<ApplicationUsageDocument> findByAppKey(String appKey);
 }
