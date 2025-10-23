db801ec25e0e68cd41f3838b0112a7281de74e39
tonyadji <tonysimonadji@gmail.com>
2025-10-23 23:56:53 +0200

    refactor(All): migrate to Java 21, enhance configuration, and streamline Docker setup
    - java 21
    - Consolidated YAML configuration with placeholders for enhanced environment variable support.
    - Refactored CORS and security configuration to use simpler string-based values processed into lists.
    - Added Dockerfile to build and run the backend application

--- backend/src/main/java/com/langa/backend/infra/security/config/SecurityEndpoints.java
+++ backend/src/main/java/com/langa/backend/infra/security/config/SecurityEndpoints.java
@@ -1,5 +1,6 @@
 package com.langa.backend.infra.security.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "application.security.endpoints")
 public class SecurityEndpoints {
 
+    @Value("${application.security.endpoints.unsecured}")
     private String unsecured;
 
     public String[] getUnsecured() { return unsecured.split(","); }
