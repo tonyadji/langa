d3a1922108071f203dbf3635c1c90693c60d6fcf
tonyadji <tonysimonadji@gmail.com>
2025-11-01 11:24:38 +0100

    refactor(Agent): enhance logging framework compatibility and streamline appender setup (#3)
    - Replaced `LangaAppender` with dedicated `LangaLog4jAppender` and `LangaLogbackAppender` for improved logging compatibility.
    - Refactored `AgentInit` to dynamically bind the appropriate appender based on the logging framework (Logback or Log4j).
    - Enhanced `SenderServiceFactory` with simplified and modular ingestion parameter handling.
    - Centralized buffer initialization within `AgentInit` for cleaner appender logic.

---
+++ agent/src/main/java/com/capricedumardi/agent/core/appenders/LangaLog4jAppender.java
@@ -0,0 +1,51 @@
+package com.capricedumardi.agent.core.appenders;
+
+import com.capricedumardi.agent.core.buffers.BuffersFactory;
+import com.capricedumardi.agent.core.buffers.GenericBuffer;
+import com.capricedumardi.agent.core.model.LogEntry;
+import com.capricedumardi.agent.core.model.SendableRequestDto;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.*;
+import org.apache.logging.log4j.core.appender.AbstractAppender;
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+
+import java.io.Serializable;
+
+@Plugin(name = "LangaAppender",
+        category = Core.CATEGORY_NAME,
+        elementType = Appender.ELEMENT_TYPE,
+        printObject = true)
+public class LangaLog4jAppender extends AbstractAppender {
+    private static final Logger log = LogManager.getLogger(LangaLog4jAppender.class);
+    private final GenericBuffer<LogEntry, SendableRequestDto> logBuffer;
+
+    public LangaLog4jAppender(String name, Filter filter, Layout<? extends Serializable> layout) {
+        super(name, filter, layout, true, null);
+        this.logBuffer = BuffersFactory.getLogBufferInstance();
+    }
+
+
+    @Override
+    public void append(LogEvent event) {
+        String message = event.getMessage().getFormattedMessage();
+        String level = event.getLevel().toString();
+        String loggerName = event.getLoggerName();
+        String timestamp = String.valueOf(event.getTimeMillis());
+
+        try {
+            LogEntry entry = new LogEntry(message, level, loggerName, timestamp);
+            log.trace("Adding entry to log buffer");
+            logBuffer.add(entry);
+
+        } catch (Exception e) {
+            log.error("Error sending log to Langa: {}", e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+        logBuffer.shutdown();
+    }
+}
--- agent/src/main/java/com/capricedumardi/agent/core/config/AgentInit.java
+++ agent/src/main/java/com/capricedumardi/agent/core/config/AgentInit.java
@@ -1,22 +1,54 @@
 package com.capricedumardi.agent.core.config;
 
+import com.capricedumardi.agent.core.buffers.BuffersFactory;
+import com.capricedumardi.agent.core.helpers.EnvironmentUtils;
+import com.capricedumardi.agent.core.helpers.IngestionParamsResolver;
+import com.capricedumardi.agent.core.services.SenderService;
+import com.capricedumardi.agent.core.services.SenderServiceFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.weaver.loadtime.Agent;
+
 import java.lang.instrument.Instrumentation;
 
 public class AgentInit {
+
+    private static final Logger log = LogManager.getLogger(AgentInit.class);
+    private static final int DEFAULT_BATCH_SIZE = 50;
+    private static final int DEFAULT_FLUSH_DELAY_IN_SECONDS = 5;
+
+    private AgentInit() {}
+
     public static void premain(String agentArgs, Instrumentation inst) {
-        System.out.println("== MonitoringAgent loaded ==");
+        System.out.println("== Langa Agent Init ==");
+
+        try {
+            initSenderAndBuffers();
+        } catch (Exception e) {
+            log.error("FATAL: Could not initialize sender/buffers. Agent logging disabled.", e);
+            return;
+        }
 
         if (isSpringPresent()) {
-            System.out.println("Spring detected → expecting @EnableAspectJAutoProxy");
+            AppenderBinding.withLogBackAppender().bind();
+            log.info("Spring detected → expecting @EnableAspectJAutoProxy");
         } else {
-            System.out.println("Spring NOT detected → using AspectJ weaver");
+            AppenderBinding.withLog4jAppender().bind();
+            log.info("Spring NOT detected → using AspectJ weaver");
             try {
-                //TODO : fix this
-                //org.aspectj.weaver.loadtime.Agent.premain(agentArgs, inst);
+                Agent.premain(agentArgs, inst);
             } catch (Exception e) {
+                log.error("Unable to init agent : {}", e.getMessage(), e);
                 e.printStackTrace();
             }
         }
+    }
+
+    private static void initSenderAndBuffers() {
+        IngestionParamsResolver resolver = EnvironmentUtils.getIngestionParamsResolver();
+        SenderService senderService = SenderServiceFactory.create(resolver);
+        BuffersFactory.init(senderService, resolver.resolveAppKey(), resolver.resolveAccountKey(),
+                DEFAULT_BATCH_SIZE, DEFAULT_FLUSH_DELAY_IN_SECONDS);
     }
 
     private static boolean isSpringPresent() {
