80bb9315a07763ec2c3eb0d96735d99349771445
tonyadji <tonysimonadji@gmail.com>
2026-01-04 13:30:27 +0100

    refactor(app usage): adding usage in secured details

--- backend/src/main/java/com/langa/backend/infra/rest/applications/dto/ApplicationUsageDto.java
+++ backend/src/main/java/com/langa/backend/infra/rest/applications/dto/ApplicationUsageDto.java
@@ -1,5 +1,6 @@
 package com.langa.backend.infra.rest.applications.dto;
 
+import com.langa.backend.domain.applications.Application;
 import com.langa.backend.domain.applications.valueobjects.ApplicationUsageInfo;
 
 public record ApplicationUsageDto(
@@ -11,6 +12,20 @@ public record ApplicationUsageDto(
 ) {
 
     public static ApplicationUsageDto of(ApplicationUsageInfo usage) {
+        if (usage == null) {
+            return ApplicationUsageDto.empty();
+        }
         return new ApplicationUsageDto(usage.id(), usage.key(), usage.name(), usage.logSize(), usage.metricSize());
+    }
+
+    private static ApplicationUsageDto empty() {
+        return new ApplicationUsageDto(null, null, null, 0L, 0L);
+    }
+
+    public static ApplicationUsageDto of(Application app) {
+        if (app.getUsage() != null) {
+            return  new ApplicationUsageDto(app.getId(), app.getKey(), app.getName(), app.getUsage().totalLogBytes(), app.getUsage().totalMetricBytes());
+        }
+        return  new ApplicationUsageDto(app.getId(), app.getKey(), app.getName(), 0L, 0L);
     }
 }
